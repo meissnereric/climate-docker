ea668a9712b351fb8bb051827c55f77840d8fe7a
mala <mala.virdee@gmail.com>
2022-05-03 17:39:43 +0100

    update select_location

--- app/climate_tasks.py
+++ app/climate_tasks.py
@@ -1,55 +1,45 @@
 from utils import *
 import numpy as np
 
-class ClimateModel:
 
-    def __init__(self, name, data=None):
-        self.name = name
-        self.data = data
-        # TODO Add all the rest of the models here / structured as needed.
-        if name == "t2m":
-            self.data.df = normalize_time(self.data.df).sel(bnds=0)
-        elif name =='EC_Earth':
-            self.data.df = normalize_time(self.data.df).rename({'lat': 'latitude','lon': 'longitude'}).sel(bnds=0)
-        elif name =='GFDL_ESM4':
-            self.data.df = normalize_time(cfnoleap_to_datetime(self.data.df)).rename({'lat': 'latitude','lon': 'longitude'}).sel(bnds=1)
-        else:
-            print("ERROR: Please  pass the type of model.")
+def select_locations(data, parameters):
+    print('Selecting location...')
 
-## Coordinates
-## Different methods can be used to choose coordinate places (nearest is default)
-def select_location(data, parameters):
-    """
-    :param data: Data type object
-    """
-    print("Made it inside select_location call!")
-    isERA5=False # parameters?
-    start='1979-01-01'
-    end='2014-12-31'
+    # ensure time range is same across models
+    start, end = parameters['time_range']
+
     coordinates = {}
-    for l in parameters['locations']:
-        coordinates[l]=get_coords(l)
-    models = {}
-    for model_name, model_data in data['models'].items():
+    for city in parameters['cities']:
+        # this is redundant since get_1d takes city:str ?
+        coordinates[city] = get_coords(city) # returns lat, lon
+        print("***************"), city)
 
-        model = ClimateModel("EC_Earth", data=model_data)
-        data = model.data.df
-        print("**********************************select_location data***************************", data)
+    # for now, move reference (ERA5) location selection elsewhere
+    model = {}
+    for name, data in parameters['models'].items():
+
+        model_name = name
+        model_data = data
+        model_df = data.df #?
+
+        print("***************"), model_name, model_df.head())
 
         start_date = np.datetime64(start)
         end_date = np.datetime64(end)
-        for coordinate in coordinates.values():
-            # note that longitudes are 0->360 not -180->+180
-            if isERA5:
-                location_data = select_time(data, start_date, end_date).sel(
-                    latitude=coordinate[0], longitude=180+coordinate[1], method='nearest').t2m.to_dataframe()
-            else:
-                location_data = select_time(data, start_date, end_date).sel(
-                    latitude=coordinate[0], longitude=180+coordinate[1], method='nearest').tas.to_dataframe()
-            models[model_name] = location_data
-    # return models
 
-    return {'s3://climate-ensembling/tst/EC-Earth3/': pd.DataFrame(np.ones((10,10)))}
+        for coord in coordinates.values():
+
+            model_1d = get_1d(model_data, city=city, start=start_date, end=end_date)
+            model_1d_df = model_1d.df #?
+
+            print("***************"), model_1d_df.head())
+
+            models[model_name]=model_1d_df
+
+        return {'s3://climate-ensembling/tst/EC-Earth3/': pd.DataFrame(np.ones((10,10)))}
+        #path = 's3://climate-ensembling/' + ...
+        #return {path: models}
+
 
 def apply_bias_correction(era5_model_data, model_data, bc_method,
                             past_start='1979-01-01', past_end='2000-12-31',
