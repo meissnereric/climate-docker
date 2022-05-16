7bdf0aa6a4e474e7f37f22d30f098034f100654b
mala <mala.virdee@gmail.com>
2022-05-16 17:28:17 +0100

    update climate_tasks

--- app/climate_tasks.py
+++ app/climate_tasks.py
@@ -24,16 +24,18 @@ def process_data(data, parameters):
             processed_data.attrs["is_reference"]==True
             models[model_name] = processed_data
 
+            # *** save reference dataset to s3 here, so that following functions can be per model
+
         else:
             processed_data = process_models(model_data, standardised_calendar)
             processed_data.attrs["is_reference"]==False
             models[model_name] = processed_data
 
-    #return models
-    return {'s3://climate-ensembling/tst/EC-Earth3/': pd.DataFrame(np.ones((10,10)))}
+    return models
+    #return {'s3://climate-ensembling/tst/EC-Earth3/': pd.DataFrame(np.ones((10,10)))}
 
 
-def select_location(data, parameters):
+def select_location(models, parameters):
     '''
     Select data by locations and time ranges specified in parameters
     '''
@@ -42,68 +44,76 @@ def select_location(data, parameters):
     start = parameters['time_range'][0] # np.datetime64
     end = parameters['time_range'][1]
 
-    models = {}
+    models_selection = {}
 
-    for model_name, model_data in data["models"].items():
+    for model_name, model_data in models.items():
         for loc in parameters['locations']:
             selected_data = select_location(model_data, loc, start, end)
 
-            models[model_name] = selected_data
+            models_selection[model_name] = selected_data
 
-    #return models
-    return {'s3://climate-ensembling/tst/EC-Earth3/': pd.DataFrame(np.ones((10,10)))}
+    return models_selection
+    #return {'s3://climate-ensembling/tst/EC-Earth3/': pd.DataFrame(np.ones((10,10)))}
 
     #path = 's3://climate-ensembling/' + ...?
     #return {path: models}
 
+def apply_bias_correction(models, parameters):
+    '''
+    Take in models_selection dict i.e. 1-d, standardised time-range
+    and bias-correction method specified in parameters
+    returns dict of bias-corrected dataframes + reference data
+    '''
 
-#**************************************************************************************
+    reference = xr.open_dataset("<path_to_reference_dataset>", engine="netcdf4")
 
+    models_to_correct={}
+    for model_name, model_data in models.items():
+        if model_data.is_reference==True:
+            continue # the reference dataset was previously saved
 
-def apply_bias_correction(model_data, reference_data, bias_corrector, past, future):
-    '''apply a specified bias correction method (incl. None)'''
-    model = model_data.df
-    reference = reference.df
+        elif model_data.is_reference==False:
+            models_to_correct[model_name]=model_data
 
-    model_past = select_time(model, reference.time[0], reference.time[-1])
+    past = parameters['past']
+    future = parameters['future']
 
+    bias_corrected_models={}
+    for model_name, model_data in models_to_correct.items():
+        if parameters[bias_correction_method]=="none":
+            bias_corrected_models[model_name] = no_correction(model_name, model_data, reference, past, future)
 
-def apply_bias_correction(era5_model_data, model_data, bc_method,
-                            past_start='1979-01-01', past_end='2000-12-31',
-                            future_start='2000-01-01', future_end='2014-12-31'):
+        elif parameters[bias_correction_method]=="delta":
+            bias_corrected_models[model_name] = delta_correction(model_name, model_data, reference, past, future)
 
-    model = ClimateModel("climate_model", data=model_data)
-    era5_model = ClimateModel("climate_model", data=era5_model_data)
-    data = model.data.df
-    era5 = era5_model.data.df
+        #elif etc for other b-c methods
 
-    ps_past = [np.datetime64(past_start), np.datetime64(past_end)]
-    ps_future = [np.datetime64(future_start), np.datetime64(future_end)]
+    return bias_corrected_models
 
-    ERA5_past = era5.loc[ps_past[0]:ps_past[1]].t2m
+def compute_disruption_days(models, parameters):
+    '''
+    ...
+    '''
 
-    past = data.loc[ps_past[0]:ps_past[1]].tas
-    future = data.loc[ps_future[0]:ps_future[1]].tas
+    temperature_threshold = parameters['temperature_threshold']
 
-    if bc_method == 'delta':
-        corrected = delta_correction(ERA5_past, past, future)
+    disruption_days={}
+    for model_name, model_df in models.items():
+        model_df['exc_{}'.format(temperature_threshold)] = np.where(model_df.model >= temperature_threshold, 1, 0)
+        disruption_days[model_name] = model_df
 
-    return {'bias_corrected': corrected.to_pandas()}
+    return disruption_days
 
-def compute_disruption_days(model, temperature_threshold):
-    data = model.data.df
+def aggregate_models(models, parameters):
+    '''
+    ...
+    '''
 
-    celsius = temperature_threshold - 273.13
-    EC_Earth_df = data.to_frame()
-    EC_Earth_df['exc_{}'.format(celsius)] = np.where(data['tas'] >= temperature_threshold, 1,0)
-    return {'disruption_days': EC_Earth_df.to_pandas()}
+    temperature_threshold = parameters['temperature_threshold']
 
-def aggregate_models(EC_Earth, models, temp=30):
-    data = model.data.df
-    annual_exc = EC_Earth['exc_{}'.format(temp)].groupby(EC_Earth.index.year).sum().to_frame('EC_Earth')
-    for model in models:
-        # groupby year, aggregate count on 'exc'
-        annual_exc[model.name] = data['exc_{}'.format(temp)].groupby(data.index.year).sum().to_frame()
+    annual_exc={}
+    for model_name, model_df in models.items():
+        annual_exc[model_name] = model_df['exc_{}'.format(temperature_threshold)].groupby(model_df.index.year).sum().to_frame(model_name)
 
-    annual_exc['ensemble_mean_{}'.format(temp)] = annual_exc.mean(axis=1)
-    return {'aggregated_results': annual_exc.to_pandas()}
+    annual_exc['aggregate_mean_{}'.format(temperature_threshold)] = annual_exc.mean(axis=1).to_pandas()
+    return annual_exc
