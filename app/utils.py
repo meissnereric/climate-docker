5fd4ae2eb244b832373477d128ae595c9e410bfd
mala <mala.virdee@gmail.com>
2022-05-16 14:12:09 +0100

    update utils

--- app/utils.py
+++ app/utils.py
@@ -6,7 +6,7 @@ import calendar
 import os
 from geopy.geocoders import Nominatim
 
-###### import data
+##################### import data #####################
 
 def get_local_directory():
     cwd = os.getcwd()
@@ -34,11 +34,11 @@ def import_reference(folder="reference/ERA5"):
     return xr.open_mfdataset(path, engine="netcdf4")
 
 
-###### data processing
+##################### data processing #####################
 
 def remove_feb_29_30(ds):
     '''
-    if dataset contains Feb 29 or Feb 30, it cannot be converted between calendar formats,
+    if time index contains Feb 29 or Feb 30 as in some cftime formats, it cannot be converted,
     therefore these are removed from all
     '''
     feb_29 = ds.time.values[(ds.time.dt.month == 2) & (ds.time.dt.day == 29)]
@@ -58,6 +58,16 @@ def calendar_type(ds):
     '''
     return type(ds.time.values[0])
 
+def rename_coord(ds, names_dict):
+    '''
+    Rename coordinates in xr Dataset
+    takes dictionary {"old_name":"new_name"}
+    '''
+    for old_name, new_name in names_dict.items():
+        if old_name in list(ds.coords.keys()):
+            ds.rename({old_name:new_name})
+    return ds
+
 def cf_to_datetime(ds):
     '''
     take cftime calendar types and convert to datetime64
@@ -75,6 +85,9 @@ def process_reference(ds):
     '''
     a = remove_feb_29_30(ds)
     b = normalize_time(a)
+    # rename ERA 2m surface temperature "t2m" to temperature at surface "tas", as is model convention
+    names_dict = {"latitude":"lat", "longitude":"lon", "t2m":"tas"}
+    c = rename_coord(b, names_dict)
     return b
 
 def process_models(ds, reference):
@@ -89,6 +102,7 @@ def process_models(ds, reference):
     if calendar_type(b) in {np.datetime64}:
         c = b
     elif calendar_type(b) in {cftime._cftime.DatetimeNoLeap, cftime._cftime.Datetime360Day}:
+        # most common types in cmip5/cmip6 - may need to search for any others
         c = cf_to_datetime(b)
     else:
         print("Error: unknown calendar type")
@@ -96,75 +110,90 @@ def process_models(ds, reference):
     e = normalize_time(d)
     return e
 
-######
+##################### data selection #####################
 
+def get_coords(city:str):
+    '''
+    return lat, lon for city
+    see: https://geopy.readthedocs.io/en/stable/#nominatim
+    '''
+    geolocator = Nominatim(user_agent='http')
+    location = geolocator.geocode(city)
+    latitude, longitude = location.latitude, location.longitude
+    #print(location, (latitude, longitude))
+    return (latitude, longitude)
 
-#**************************************************************************************
+def select_time(ds, start, end):
+    '''
+    select time range from np.datetime start and end dates e.g. np.datetime64('1999-01-31')
+    returns Dataset
+    '''
+    return ds.sel(time=slice(start, end))
 
+def select_location(ds, city, start=None, end=None):
+    '''
+    select 1-d time series for specified city, optionally select time range
+    returns DataArray
+    '''
+    coords = get_coords(city)
+    # note: model + ERA latitudes are -90 -> +90, longitudes are 0 -> +360
+    ds_1d = ds.sel(lat=coords[0], lon=180+coords[1], method='nearest')
+    ds_1d_sl = select_time(model_1d, start, end)
+    return(ds_1d_sl)
 
-
-
-def cfnoleap_to_datetime(ds):
-    datetimeindex = ds.indexes['time'].to_datetimeindex()
-    #ds = da.to_dataset()
-    ds_dt=ds
-    ds_dt['time_dt']= ('time', datetimeindex)
-    ds_dt = ds_dt.swap_dims({'time': 'time_dt'})
-    assert len(ds.time) == len(ds_dt.time_dt)
-    return ds_dt.set_index(time='time_dt')
+def get_nearest(ds, latitude, longitude):
+    '''
+    get grid-point nearest to specified lat, lon
+    (i.e. for checking how far city coordinate is from closest grid-point in dataset)
+    '''
+    lat = ds.sel(lat=latitude, lon=longitude, method="nearest")['lat'].values
+    lon = ds.sel(lat=latitude, lon=longitude, method="nearest")['lon'].values
+    return float(lat), float(lon)
 
 def rename_coord(da, old_name, new_name):
-    ''' Rename coordinate in xr data array
+    '''
+    rename coordinate in xr data array
     '''
     if old_name in list(da.coords.keys()):
         da = da.rename({old_name:new_name})
     return da
 
-def get_nearest(da, latitude, longitude):
+
+##################### calendar utils #####################
+
+def calendar_check(da_1, da_2):
     '''
-    get point nearest to specified lat, lon
+    check that 2 DataArrays contain same calendar dates
     '''
-    lat = da.sel(lat=latitude, lon=longitude, method="nearest")['lat'].values
-    lon = da.sel(lat=latitude, lon=longitude, method="nearest")['lon'].values
-    return float(lat), float(lon)
+    m = set(da_1.index).symmetric_difference(set(da_2.index))
+    if len(m)>0:
+        mismatch = True
+    elif len(m) == 0:
+        mismatch = False
+    return(mismatch, m)
 
-def import_mdf_dataset(directory, folder):
-    print("Import dataset: dir {} folder {}".format(directory, folder))
-    path = os.path.join(directory+"/"+folder+'/*.nc')
-    return xr.open_mfdataset(path, engine="netcdf4")
+def calendar_days(start, end):
+    '''
+    return real number of days between two np.datetime64 dates
+    (i.e. to check how many days have been removed from data during standardisation)
+    '''
+    delta = end - start
+    return(delta.astype('timedelta64[D]'))
 
-def cfnoleap_to_datetime(ds):
-    '''some models use cftimeindex - need to convert these to datetimeindex'''
-    datetimeindex = ds.indexes['time'].to_datetimeindex()
-    #ds = da.to_dataset()
-    ds_dt=ds
-    ds_dt['time']= ('time', datetimeindex)
-    assert len(ds.time) == len(ds_dt.time)
-    return ds_dt
+##################### bias correction #####################
 
-def normalize_time(ds):
-    '''remove times from daily-averaged data'''
-    ds['time'] = ds.indexes['time'].normalize()
-    return ds
+#**************************************************************************************
 
-def get_coords(city):
-    '''return lat, lon for city (string)'''
-    geolocator = Nominatim(user_agent='http')
-    location = geolocator.geocode(city)
-    latitude, longitude = location.latitude, location.longitude
-    print(location, (latitude, longitude))
-    return (latitude, longitude)
-
-def select_time(ds, start, end):
-    return ds.sel(time=slice(start, end))
-
-
-# #### 2. For each of the models used, at each location, a bias correction to the raw data is calculated based on the observational data. This defines a transfer function that is then used on model predictions to bias-correct each model's future output.
 def delta_correction(observation, model_past, model_future):
-    '''simplest mean-shift delta bias correction method
-    adding the mean change signal to observations'''
+    '''
+    simplest mean-shift delta bias correction method
+    adding the mean change signal to observations
+    '''
     c = model_future + (np.nanmean(observation) - np.nanmean(model_past))
     return c
+
+
+
 
 ######################## Utils for Task Management #######################
 def load_object_from_s3(s3_client):
